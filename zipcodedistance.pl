5c91704527b96e30c5da0d2c18e88522d8a8422a
Trayton White <tbw@traytonwhite.com>
2013-05-08 00:19:52 -0700

    added first subroutine file skeleton

---
+++ zipcodedistance.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: zipcodedistance.pl
+#
+#        USAGE: ./zipcodedistance.pl
+#
+#  DESCRIPTION: Subroutine here calculates distance between 2 zip codes
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Trayton White (tbw), tbw@traytonwhite.com
+# ORGANIZATION:
+#      VERSION: 1.0
+#      CREATED: 05/08/2013 12:13:39 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+use Math::Trig qw( great_circle_distance deg2rad );
+
+sub zipdistance {
+    my  ( $par1 )   = @_;
+    return ;
+} ## --- end sub zipdistance
