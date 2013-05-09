10ca9d16a921ac826c8707c35faf40c11960565e
Trayton White <tbw@traytonwhite.com>
2013-05-09 01:08:05 -0700

    First draft of distance calculation function appears to work as expected

--- zipcodedistance.pl
+++ zipcodedistance.pl
@@ -20,10 +20,47 @@
 
 use strict;
 use warnings;
-
 use Math::Trig qw( great_circle_distance deg2rad );
+use Data::Dumper;
 
 sub zipdistance {
-    my  ( $par1 )   = @_;
-    return ;
+    my  ( $ziplatlonhash, $zipcode, $ziplist, $radius ) = @_;
+    my %returnhash;
+
+    for ( @$ziplist ) {
+        if ( $zipcode gt $_ ) {
+            next;
+        } elsif ( $zipcode eq $_ ) {
+            $returnhash{$zipcode}{$_} = 0;
+        } else {
+            $returnhash{$zipcode}{$_} = great_circle_distance(
+                ${$ziplatlonhash}{$zipcode}{'lon'},
+                90 - ${$ziplatlonhash}{$zipcode}{'lat'},
+                ${$ziplatlonhash}{$_}{'lon'},
+                90 - ${$ziplatlonhash}{$_}{'lat'},
+                $radius
+            )
+        }
+    }
+    return \%returnhash;
 } ## --- end sub zipdistance
+
+
+my %zipdb = (
+        '94123' => {
+            'lat' => 37.8,
+            'lon' => -122.4,
+        },
+        '46714' => {
+            'lat' => 40.7,
+            'lon' => -85.2,
+        },
+    );
+my @listofzips = ( 94123, 46714 );
+
+my $final = zipdistance(\%zipdb, '46714', \@listofzips, 6378);
+
+print "${$final}{'46714'}{'94123'} \n";
+
+
+
