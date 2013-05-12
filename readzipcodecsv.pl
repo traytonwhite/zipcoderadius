de654e7a304576da4ab3fb5b08bf1758020717b2
Trayton White <tbw@traytonwhite.com>
2013-05-11 22:47:02 -0700

    adding file for reading in zip code csv file

---
+++ readzipcodecsv.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: readzipcodecsv.pl
+#
+#        USAGE: ./readzipcodecsv.pl
+#
+#  DESCRIPTION: Read the zip code CSV file to load into PostgreSQL DB
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Trayton White (tbw), tbw@traytonwhite.com
+# ORGANIZATION:
+#      VERSION: 1.0
+#      CREATED: 05/11/2013 10:44:26 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
